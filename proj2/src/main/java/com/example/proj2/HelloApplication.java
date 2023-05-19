69146db417c43374a80a26a7ef27e699a4deb25d
HumbertodeCastro <h.decastro@outlook.com>
2023-05-19 17:51:01 +0200

    hover css geadd aan buttons

--- proj2/src/main/java/com/example/proj2/HelloApplication.java
+++ proj2/src/main/java/com/example/proj2/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.proj2;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,8 +13,11 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Image icon = new Image("C:\\Users\\Humberto de Castro\\OneDrive\\Documents\\GitHub\\PROJ\\Proj2\\proj2\\src\\42.jpg");
+        stage.getIcons().add(icon);
         stage.setTitle("AIsistify");
         stage.setScene(scene);
+        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm()); // voor hover effect
         stage.show();
     }
 
